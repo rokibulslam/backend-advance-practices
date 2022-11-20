2a867f929cc08468a800f505d491c5c74c760f21
rokibulslam <rokibulslam6@gmail.com>
2022-11-20 20:23:39 +0600

    basic url

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const http = require('http');
 
 const server = http.createServer(function (req, res) {
-    res.end('hello node.js');
+    // console.log(req.url)
+    if (req.url == '/') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({name:"Rokibul Islam"}));    //sending JSON Data
+        res.end();
+    }
+    else if (req.url == '/contact') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write("<H1>This is Contact Page</H1>");   //sending HTML
+        res.end();
+    };
+    if (req.url == '/about') {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<H1>This is About-Us Page</H1>");
+        res.end();
+    }
 });
 
 const PORT = 5000;
